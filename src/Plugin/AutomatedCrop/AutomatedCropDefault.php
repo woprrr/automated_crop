6fba6a8d22ea333d86ca561b6d5ad9ed45da6efe
Rosemary Stanley <rosemary@interactiveknowledge.com>
2021-11-01 06:27:40 -0400

    Issue #3023477: Aspect ratio calculation is slightly off and Issue #3023475 by rosemarystanley: Initial crop not setting as expected

--- src/Plugin/AutomatedCrop/AutomatedCropDefault.php
+++ src/Plugin/AutomatedCrop/AutomatedCropDefault.php
@@ -29,21 +29,11 @@ final class AutomatedCropDefault extends AbstractAutomatedCrop {
    * {@inheritdoc}
    */
   public function calculateCropBoxSize() {
-    if (!$this->hasSizes() && !$this->hasHardSizes()) {
-      $this->automatedCropBoxCalculation();
-    }
+    $this->automatedCropBoxCalculation();
+    
+    $width = $this->cropBox['width'];
+    $height = $this->cropBox['height'];
 
-    if ('width' === $this->findUnknownValue()) {
-      $this->calculateUnknownValue($this->cropBox['width']);
-    }
-
-    if ('height' === $this->findUnknownValue()) {
-      $this->calculateUnknownValue($this->cropBox['height']);
-    }
-
-    // Initialize auto crop area & unsure we can't exceed original image sizes.
-    $width = min(max($this->cropBox['width'], $this->cropBox['min_width']), $this->cropBox['max_width']);
-    $height = min(max($this->cropBox['height'], $this->cropBox['min_height']), $this->cropBox['max_height']);
     $this->setCropBoxSize($width, $height);
 
     return $this;
@@ -63,57 +53,34 @@ final class AutomatedCropDefault extends AbstractAutomatedCrop {
    * configurations of max width/height.
    */
   protected function automatedCropBoxCalculation() {
-    $delta = $this->getDelta();
+    // $delta = $this->getDelta();
     $width = $this->originalImageSizes['width'];
-    $height = round(($width * $delta));
+    $height = $this->originalImageSizes['height'];
+    $aspectWidth = $width;
+    $aspectHeight = $height;
 
-    if (!empty($this->cropBox['max_height']) && $height > $this->cropBox['max_height']) {
-      $height = $this->cropBox['max_height'];
-      $width = round(($height * $delta));
+    if (!empty($this->cropBox['aspect_ratio'])) {
+      $measures = explode(':', $this->cropBox['aspect_ratio']);
+      $aspectWidth = $measures[0];
+      $aspectHeight = $measures[1];
     }
 
-    if (!empty($this->cropBox['max_width']) && $width > $this->cropBox['max_width']) {
-      $width = $this->cropBox['max_width'];
-      $height = round(($width * $delta));
+    $aspectRatio = $aspectWidth / $aspectHeight;
+
+    // Vertical.
+    if ($aspectRatio < 1) {
+      $newWidth = min($height * $aspectRatio, $width);
+      $newHeight = $newWidth / $aspectRatio;
+
+    // Horizontal.
+    } else {
+      $newHeight = min($width / $aspectRatio, $height);
+      $newWidth = $newHeight * $aspectRatio;
+  
     }
 
-    $this->cropBox['width'] = $width;
-    $this->cropBox['height'] = $height;
-  }
-
-  /**
-   * Evaluate if with or height need to be calculated.
-   *
-   * If we have already ALL cropBox sizes we just need to apply,
-   * it don't need to evaluate missing values.
-   *
-   * @return bool|string
-   *   The value to find or False if cropBox have any sizes found.
-   */
-  protected function findUnknownValue() {
-    if (!$this->hasSizes()) {
-      return FALSE;
-    }
-
-    $valueToSearch = 'width';
-    if (!empty($this->cropBox['width']) && empty($this->cropBox['height'])) {
-      $valueToSearch = 'height';
-    }
-
-    return $valueToSearch;
-  }
-
-  /**
-   * Calculate the new value of given width or height respecting homothety.
-   *
-   * @param int $value
-   *   Value to convert with image delta to found compatible new sizes.
-   *
-   * @return int
-   *   The new height or width respect the homothety of image.
-   */
-  protected function calculateUnknownValue($value) {
-    return round(($value * $this->delta));
+    $this->cropBox['width'] = $newWidth;
+    $this->cropBox['height'] = $newHeight;
   }
 
 }
